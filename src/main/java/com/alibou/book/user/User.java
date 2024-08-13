3129b046cb251a8620b2c60cf5c4ca24a2189deb
chunkityip <alex0917lfo@hotmail.com>
2024-08-13 05:33:49 -0400

    feat: create the Token entity and repository

--- src/main/java/com/alibou/book/user/User.java
+++ src/main/java/com/alibou/book/user/User.java
@@ -1,12 +1,13 @@
 package com.alibou.book.user;
 
+import com.alibou.book.role.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Principal;
@@ -14,7 +15,16 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static jakarta.persistence.FetchType.EAGER;
+
+
+/**
+ * @EntityListeners(AuditingEntityListener.class) will auto handle the auditing
+ * process whenever an entity undergoes lifecycle events such as persist, update
+ * or remove
+ */
 @Getter
 @Setter
 @Builder
@@ -22,11 +32,6 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Table(name = "user")
-/**
- * @EntityListeners(AuditingEntityListener.class) will auto handle the auditing
- * process whenever an entity undergoes lifecycle events such as persist , update
- * or remove.
- */
 @EntityListeners(AuditingEntityListener.class)
 public class User implements UserDetails, Principal {
 
@@ -35,8 +40,7 @@ public class User implements UserDetails, Principal {
     private Integer id;
     private String firstname;
     private String lastname;
-    private LocalDate dataOfBirth;
-    // The email not allow to duplicate
+    private LocalDate dateOfBirth;
     @Column(unique = true)
     private String email;
     private String password;
@@ -46,10 +50,14 @@ public class User implements UserDetails, Principal {
     @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdDate;
+
     // Capturing date and time when an entity was last updated
     @LastModifiedDate
     @Column(insertable = false)
     private LocalDateTime lastModifiedDate;
+
+    @ManyToMany(fetch = EAGER)
+    private List<Role> roles;
 
 
     @Override
@@ -59,7 +67,10 @@ public class User implements UserDetails, Principal {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return this.roles
+                .stream()
+                .map(r -> new SimpleGrantedAuthority(r.getName()))
+                .collect(Collectors.toList());
     }
 
     @Override
