be1433527b62bbf27b6650d59bc4e693a39f2020
chunkityip <alex0917lfo@hotmail.com>
2024-08-15 05:54:28 -0400

    still working on JwtService

--- src/main/java/com/alibou/book/security/JwtService.java
+++ src/main/java/com/alibou/book/security/JwtService.java
@@ -1,10 +1,23 @@
 package com.alibou.book.security;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Service
 
 public class JwtService {
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>() , userDetails);
+    }
+
+    private  String generateToken(Map<String, Object> claims, UserDetails userDetails) {
+        return buildToken(claims, userDetails, jwtExpiration);
+    }
 
 
 }
